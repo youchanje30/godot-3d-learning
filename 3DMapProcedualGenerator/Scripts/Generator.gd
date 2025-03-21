09579d09e6cdda1adf04e8f13f1c0d0ddc6e243d
psk <77984774+youchanje30@users.noreply.github.com>
2025-03-21 23:35:45 +0900

    방 제작 및 생성 준비
    생성 준비

---
+++ 3DMapProcedualGenerator/Scripts/Generator.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+@export var generate_rooms_cnt : int = 10
+var q = []
+
+
+func _init() -> void:
+	q.push_back(Vector3.ZERO)
+
+
+
+
+func GenerateMaps()->void:
+	pass
+	
---
+++ 3DMapProcedualGenerator/Scripts/RoomData.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@export var size : Vector3
+
+
+func GetSize()->Vector3:
+	return size
