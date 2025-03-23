6260728155ee0c51469fe5ef7930cd772a0dfcf5
youchanje30 <77984774+youchanje30@users.noreply.github.com>
2025-03-23 20:13:38 +0900

    안쓰는 프로젝트 제거 및 코드 리펙토링
    맵 생성 코드 정리
    안쓰는 파일들 제거
    
    시드 기반 생성으로 가기 위해 준비하는 시간

--- 3DMapProcedualGenerator/Scripts/Generator.gd
+++ 3DMapProcedualGenerator/Scripts/Generator.gd
@@ -1,48 +1,68 @@
 extends Node3D
 
-
 @export var generate_rooms_cnt : int = 10
-var q = []
-var path = "res://3DMapProcedualGenerator/Rooms/"
-
+@export var room_path : String = "res://3DMapProcedualGenerator/Rooms/"
+var q = [] # pos, angle
 
 func _init() -> void:
-	q.push_back([Vector3.ZERO, Vector3(0, 0, 1)])
+	q.push_back([Vector3.ZERO, 0])
+
+func _ready() -> void:
 	GenerateMaps()
-
-func GenerateMaps()->void:
-	var generated_cnt = 0
-	while generated_cnt < generate_rooms_cnt:
-		# 위치 3번, 방 5번 시도 해보기
-		var room_generate_succed = false
-		var try_pos_cnt = 0
-		while try_pos_cnt < 3:
-			# 위치 선택하기
-			var data = q.pick_random()
-			var pos = data[0]
-			var try_room_cnt = 0
-			while try_room_cnt < 5:
-				# 방 선택하기
-				var room = GetRandomFromPath(path)
-				add_child(room)
-				var size = room.get_size()
-				var dir = pos
-				if data[1].x != 0:
-					dir.x = (data[1].x/abs(data[1].x)) * size.x / 2 + pos.x
-				if data[1].z != 0:
-					dir.z = (data[1].z/abs(data[1].z)) * size.z / 2 + pos.z
-				
-				# dir은 정중앙임
-				# 시작 지점은 -1/2로 통일 끝 지점은 +1/2로 통일
-				var start_pos = Vector3(dir.x - size.x/2, dir.y - size.y/2, dir.z - size.z/2)
-				var end_pos = Vector3(dir.x + size.x/2, dir.y + size.y/2, dir.z + size.z/2)
-				var check = PhysicsRayQueryParameters3D.create(start_pos, end_pos)
-				
-	pass
-
 
 func GetRandomFromPath(path : String):
 	var resource_files = DirAccess.get_files_at(path)
 	var random_resource = resource_files[randi() % resource_files.size()]
 	var data = load(path + random_resource).instantiate()
 	return data
+
+func GenerateMaps() -> void:
+	for _i in generate_rooms_cnt:
+		await GenerationRoomCycle()
+		await get_tree().create_timer(0.25).timeout
+
+func GenerationRoomCycle() -> void:
+	for _try_pos in 10:
+		if await TryGenerateAndPlaceRoom(): return
+
+func TryGenerateAndPlaceRoom() -> bool:
+	var generation_data = SelectNextGenerationData()
+	if generation_data == null: return false
+
+	var attach_pos = generation_data[0]
+	var attach_angle = generation_data[1]
+
+	for _try_room in 10:
+		var room = GetRandomFromPath(room_path)
+		if not room: continue
+		if await PlaceRoom(room, attach_pos, attach_angle): return true
+
+	print("생성 실패")
+	return false
+
+func SelectNextGenerationData():
+	if not q.is_empty(): return q.pick_random()
+
+	print("큐가 비었음 어떻게?")
+	return null
+
+
+func PlaceRoom(room: Node3D, attach_pos: Vector3, attach_angle: float) -> bool:
+	add_child(room)
+	room.Attach(attach_pos, attach_angle)
+	await get_tree().create_timer(0.05).timeout
+
+	if not room.has_overlapping():
+		SuccesGenerationRoom(room, [attach_pos, attach_angle])
+		return true
+
+	room.queue_free()
+	# print("방 생성 실패 (겹침):", attach_pos, room.global_position, room.get_size())
+	return false
+
+
+func SuccesGenerationRoom(room: Node3D, data_to_remove: Array) -> void:
+	print("방 생성 성공:", room.global_position, room.get_size())
+	q.erase(data_to_remove)
+	for passage in room.get_passage_data():
+		q.append(passage)
--- 3DMapProcedualGenerator/Scripts/RoomData.gd
+++ 3DMapProcedualGenerator/Scripts/RoomData.gd
@@ -22,3 +22,14 @@ func get_passage_data():
 
 func has_overlapping()->bool:
 	return area_3d.has_overlapping_areas()
+
+func Attach(pos, angle):
+	var room_size = get_size()
+	if angle == 90 or angle == -90:
+		room_size = Vector3(room_size.z, room_size.y, room_size.x)
+	var attach_dir = Vector3(int(sin(deg_to_rad(angle))), 0, int(cos(deg_to_rad(angle))))
+	var new_room_pos = pos + attach_dir * (room_size / 2.0)
+	
+	# set angle, rotate
+	self.global_position = new_room_pos
+	set_global_rotation_degrees(Vector3(0, angle, 0))
